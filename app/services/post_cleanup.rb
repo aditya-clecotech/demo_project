94ef9729971dc6a146a9333818d508cb2622cbaa
aditya-clecotech <aditya.parihar@clecotech.com>
2025-04-11 10:42:13 +0530

    a minor change in post_cleanup.rb

--- app/services/post_cleanup.rb
+++ app/services/post_cleanup.rb
@@ -2,7 +2,7 @@ class PostCleanup
 
    def call 
       # Write query (or logic) here...
-      inactive_post = Post.where(created_at: (Date.today - 6.day).all_day) #fetching all 6days old posts
+      inactive_post = Post.where(created_at: (Date.today - 6.month).all_day) #fetching all 6days old posts
       if inactive_post.destroy_all
          AdminMailer.cleanup_email.deliver
       end 
